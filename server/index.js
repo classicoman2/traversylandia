90f0e27fbe9cef523c28379e58e39653e3d3b0f1
Toni <aamengual@cifpfbmoll.eu>
2020-12-22 09:15:37 +0100

    variable entorn amb dades mongodb atlas

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-/* User i password de atlas mongodb */
-const seguretat = require('./mongodb_atlas_user/seguretat');
+//Carrega variables entorn de fitxer .env
+require('dotenv').config();
 
 //Per evitar l'error de CORS en fer peticions des d'un altre servidor
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 /*
 Node.js body parsing middleware.
@@ -16,11 +16,10 @@ are untrusted and should be validated before trusting. For example, req.body.foo
 for example the foo property may not be there or may not be a string, and toString may not 
 be a function and instead a string or other user input.
 */
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 //Middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
 // Rutes de la API
@@ -28,18 +27,17 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 */
-const cyclists = require('./routes/api/cyclists');
+const cyclists = require("./routes/api/cyclists");
 //En cas de ruta iniciada amb  /api/cyclists, la ruta completa la processarà el fitxer superior
-app.use('/api/cyclists', cyclists);
+app.use("/api/cyclists", cyclists);
 
-
-// PRODUCTION 
-if (process.env.NODE_ENV === 'production') {
+// PRODUCTION
+if (process.env.NODE_ENV === "production") {
   //Static folder que hem creat en fer   npm run build
-  app.use(express.static(__dirname + '/public/'));
+  app.use(express.static(__dirname + "/public/"));
 
   //Handle Single Page Application :   qualsevol ruta -- xtoni
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 /**
@@ -48,9 +46,15 @@ if (process.env.NODE_ENV === 'production') {
 
 // Iniciam el servidor en el Port 5000
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servidor iniciat a http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Servidor iniciat a http://localhost:${port}`);
+  console.log(process.env.MONGOATLAS_CREDENTIALS)
+});
 
 // Base de dades 'uci_database'
-mongoose.connect(`mongodb+srv://${seguretat}@cluster0-tuhp5.mongodb.net/uci_database?retryWrites=true&w=majority`, {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGOATLAS_CREDENTIALS}@cluster0-tuhp5.mongodb.net/uci_database?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+  }
+);
